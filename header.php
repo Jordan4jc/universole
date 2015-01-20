835ce2f1fa06b397e7970661c3f175429c88cafd
Jordan R <riser.jordan@gmail.com>
2015-01-20 18:23:18 -0500

    added primary navigation

--- header.php
+++ header.php
@@ -20,13 +20,16 @@
 <body <?php body_class(); ?>>
 <div id="page">
 	<header>
-		<nav>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo">
-				<?php if ( get_theme_mod( 'universole_logo' ) ) : ?>
-				  <img src='<?php echo esc_url( get_theme_mod( 'universole_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
-				<?php else : ?>
-					<img src="<?php echo bloginfo('template_directory'); ?>/images/blue-ridge-arrangement-image-logo.png">
-				<?php endif; ?>
-			</a>
-		</nav>
+		<div class="col-full">
+			<nav>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo">
+					<?php if ( get_theme_mod( 'universole_logo' ) ) : ?>
+					  <img src='<?php echo esc_url( get_theme_mod( 'universole_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
+					<?php else : ?>
+						<img src="<?php echo bloginfo('template_directory'); ?>/images/blue-ridge-arrangement-image-logo.png">
+					<?php endif; ?>
+				</a>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav>
+		</div>
 	</header>
