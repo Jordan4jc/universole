661d44df6fc42216ce353846cffe8db50a27c6ae
Jordan R <riser.jordan@gmail.com>
2015-01-24 15:09:54 -0500

    set navigation items to correct font weight and moved fonts before wp_head

--- header.php
+++ header.php
@@ -14,9 +14,10 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
 <script src="//use.typekit.net/hgy3qzw.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
+
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
