1615ce2d405447329b8a2017cf4fcdab63251793
Jordan R <riser.jordan@gmail.com>
2015-02-02 19:16:22 -0500

    added custom sandals page to loop through all products in sandals category

---
+++ page-the-sandals.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+				do_action( 'storefront_page_before' );
+				?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+			<?php 
+			$args = array(
+			'post_type' => 'product',
+			'product_category' => 'sandals',
+			'product_status' => 'publish',
+			'posts_per_page' => -1
+			);
+			$isEven;
+			$loop = new WP_Query( $args );
+			if ( $loop->have_posts() ) {
+				while ( $loop->have_posts() ) : $loop->the_post();
+					echo '<section class="inner-grid">';
+					if($isEven) {
+						echo '<aside class="col half">';
+						the_title('<h2>','</h2>');
+						the_content();
+						echo '<a class="button blue" href="'.get_permalink().'">View in the Shop</a>';
+						echo '</aside>';
+						echo '<aside class="col half">';
+						the_post_thumbnail();
+						echo '</aside>';
+					} else {
+						echo '<aside class="col half">';
+						the_post_thumbnail();
+						echo '</aside>';
+						echo '<aside class="col half">';
+						the_title('<h2>','</h2>');
+						the_content();
+						echo '<a class="button blue" href="'.get_permalink().'">View in the Shop</a>';
+						echo '</aside>';
+					}
+					$isEven = ! $isEven;
+					echo '</section>';
+				endwhile;
+			} else {
+				echo __( 'No products found' );
+			}
+			wp_reset_postdata();
+			?>
+
+<?php get_footer(); ?>
