18403ea3e1abf270ba02a082c9fe1cf7a431ad5e
Jordan Riser <riser.jordan@gmail.com>
2015-02-20 11:12:57 -0500

    added 404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<aside class="oops">
+		<h1>Opps! That page can't be found.</h1>
+		<p>Maybe that page was moved or you typed something weong, try again or head back home if you came here by mistake.</p>
+		<a href="/" class="button blue">Head Home</a>
+	</aside>
+
+<?php get_footer(); ?>
